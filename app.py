7574840c65e39d63c32d99793e5f87c29aef0524
Jatin Varlyani <varlyanijatin88@gmail.com>
2020-03-22 19:50:04 +0530

    Created an Endpoint and added corona handler

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import json
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/bot', methods=['POST','GET'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    print(incoming_msg)
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    
+    if 'corona' in incoming_msg:
+        # return total cases
+        r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
+        if r.status_code == 200:
+            data = r.json()
+            resp = f'CONFIRMED:{data["cases"]}\nDEATHS:{data["deaths"]}\nRECOVERED:{data["recovered"]}'
+            print(resp)
+        else:
+            resp = 'I could not retrieve the results at this time, sorry.'
+        msg.body(resp)
+        responded = True
+
+    if responded == False:
+        msg.body('I only know about corona, sorry!')
+
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
